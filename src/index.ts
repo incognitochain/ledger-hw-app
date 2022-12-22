baa07f83ff98d95153fa0b8d5aedf0c96b6efd6d
darius <datle@incognito.org>
2022-12-22 17:51:19 +0700

    fix incompatibility with TransportHttp

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ export * from "./getVersion";
 export * from "./calculator";
 export * from "./decrypt";
 export * from "./trustDevice";
+
+import TransportHttp from "./hw-transport-http/withStaticURLs";
+export {
+    TransportHttp,
+}
