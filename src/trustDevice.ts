15c4bec8395aaab945394d55859229b84fb72b75
darius <datle@incognito.org>
2023-01-18 22:14:54 +0700

    handle big amount in confirmTx

--- src/trustDevice.ts
+++ src/trustDevice.ts
@@ -1,5 +1,6 @@
 import type Transport from "@ledgerhq/hw-transport";
 import { cmd } from "./constants";
+import bn from "bn.js";
 
 export  async  function trustDevice(transport: Transport) {
     let buf = Buffer.from([]);
@@ -7,18 +8,21 @@ export  async  function trustDevice(transport: Transport) {
 }
 
 
-export async function confirmTx(transport: Transport, amount: number, decimal: number, address: string) {
+export async function confirmTx(transport: Transport, amount: string, decimal: number, address: string) {
+    const bnAmt = new bn(amount);
+    const onesbyte = new bn(0xFF);
     let bufAmount = Buffer.alloc(8);
-    bufAmount[0] = (amount >> 56) & 0xFF;
-    bufAmount[1] = (amount >> 48) & 0xFF;
-    bufAmount[2] = (amount >> 40) & 0xFF;
-    bufAmount[3] = (amount >> 32) & 0xFF;
-    bufAmount[4] = (amount >> 24) & 0xFF;
-    bufAmount[5] = (amount >> 16) & 0xFF;
-    bufAmount[6] = (amount >> 8) & 0xFF;
-    bufAmount[7] = amount & 0xFF;
+    bufAmount[0] = bnAmt.shrn(56).and(onesbyte);
+    bufAmount[1] = bnAmt.shrn(48).and(onesbyte);
+    bufAmount[2] = bnAmt.shrn(40).and(onesbyte);
+    bufAmount[3] = bnAmt.shrn(32).and(onesbyte);
+    bufAmount[4] = bnAmt.shrn(24).and(onesbyte);
+    bufAmount[5] = bnAmt.shrn(16).and(onesbyte);
+    bufAmount[6] = bnAmt.shrn(8).and(onesbyte);
+    bufAmount[7] = bnAmt.and(onesbyte);
 
     let bufAddress = Buffer.from(address, "hex")
+    console.log('buffer is', bufAmount);
     let arr = [bufAmount, bufAddress]
     let buf = Buffer.concat(arr)
     return transport.send(cmd.cla, cmd.ConfirmTx, 0x00, decimal, buf)
